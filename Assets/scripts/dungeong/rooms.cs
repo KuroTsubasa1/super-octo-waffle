6c299c2e0781c390c52591a215582abcf1b34b10
KuroTsubasa1 <1-IBI@web.de>
2018-02-16 10:43:12 +0100

    added pro builder, polypiant and pro grids to the project

---
+++ Assets/scripts/dungeong/rooms.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class rooms
+{
+  
+    private int roomSizeX;
+    private int roomSizeY;
+    private int[,] Position;
+
+
+}
+
+
+ 
